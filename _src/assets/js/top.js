397085765127543f80d10d0bfcee40ea2d82080d
k_tsuji <tsuji@pnrm.co.jp>
2021-11-05 17:04:10 +0900

    TOPアニメーション追加

--- _src/assets/js/top.js
+++ _src/assets/js/top.js
@@ -1,5 +1,26 @@
 // import slider from "./modules/slider";
 
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
   // slider();
+});
+
+// h1のテキストアニメーション
+$(document).ready(function () {
+  const CLASSNAME = "-visible";
+  const TIMEOUT = 300;
+  const $target = $(".main-title");
+
+  setInterval(() => {
+    $target.addClass(CLASSNAME);
+  }, TIMEOUT * 2);
+});
+
+// Worksのhoverアニメーション
+$(function () {
+  $('botton a').mouseover(function (e) {
+    $('.works-img img').addClass("imagebig");
+  })
+  $('botton a').mouseout(function (e) {
+    $('.works-img img').removeClass("imagebig");
+  })
 });
