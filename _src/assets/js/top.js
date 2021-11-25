cf0e6c30dfb0f7bdf176b6f753cdd657ae823412
tsuji2019 <tsuji@pnrm.co.jp>
2021-11-25 17:39:39 +0900

    no message

--- _src/assets/js/top.js
+++ _src/assets/js/top.js
@@ -60,7 +60,7 @@ $(function () {
   var secArr = new Array();
   var current = -1;
   /* ここに背景色を順に記述する*/
-  var secColor = new Array('#ff7e28', '#ff7e28', '#f0ecdf', '#f0ecdf');
+  var secColor = new Array('#eb5e0b', '#eb5e0b', '#f0ecdf', '#f0ecdf');
   $('.block').each(function (i) {
     secArr[i] = $(this).offset().top;
   });
@@ -98,4 +98,19 @@ $(function () {
   $('botton a.works02').mouseout(function (e) {
     $('.works-img img.works02').removeClass("imagebig");
   })
+});
+
+// About/profile 背景画像パララックス
+$(function () {
+  var target1 = $("#parallax-01");
+  var targetPosOT1 = target1.offset().top;
+  var targetFactor = 0.5;
+  var windowH = $(window).height();
+  var scrollYStart1 = targetPosOT1 - windowH;
+  $(window).on('scroll', function () {
+    var scrollY = $(this).scrollTop();
+    if (scrollY > scrollYStart1) {
+      target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+    }
+  });
 });
